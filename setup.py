1716225be367550c94d6edd6df13910ddd8223bb
Jorge A Gallegos <kad@blegh.net>
2011-10-08 13:42:27 -0700

    Writing a setup.py for easy packaging

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- mode: python; sh-basic-offset: 4; indent-tabs-mode: nil; coding: utf-8 -*-
+# vim: tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8
+#
+
+import os
+import setuptools
+import sys
+
+
+setuptools.setup(
+    name = "clusto-ec2",
+    version = "0.3",
+    packages = setuptools.find_packages('src'),
+    author = "Ron Gorodetzky",
+    author_email = "ron@parktree.net",
+    description = "Amazon EC2 extension for clusto",
+    install_requires = [
+        #'clusto>=0.7',
+        'boto>=2.0',
+        'mako',
+    ],
+    entry_points = {
+        'console_scripts': [
+        ],
+    },
+    zip_safe = False,
+    package_dir = { '': 'src' },
+)
+

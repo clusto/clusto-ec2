7adf813a7e10fc02dafb2e59d926f6db92d3dcac
Mike Newton <mike@delusion.org>
2012-07-18 16:15:27 -0700

    add EC2VirtualMachine objects for existing instances as part of bootstrap helper script.

--- helpers/bootstrap_ec2.py
+++ helpers/bootstrap_ec2.py
@@ -95,6 +95,27 @@ class BootstrapEc2(script_helper.Script):
                 self.debug('Adding region %s to pool %s' %
                     (region.name, args.add_to_pool,))
                 container_pool.insert(region_entity)
+
+        self.info('Creating all instances')
+        for reservations in conn.get_all_instances():
+            for instance in reservations.instances:
+                instance_entity = clusto.get_or_create(instance.id,
+                        ec2_drivers.EC2VirtualServer)
+                placement = clusto.get_by_name(instance.placement)
+                if placement not in instance_entity.parents():
+                    placement.insert(instance_entity)
+                instance_entity.add_attr(key='aws', subkey='ec2_instance_type',
+                        value=instance.instance_type)
+                instance_entity.add_attr(key='aws', subkey='ec2_ami',
+                        value=instance.image_id)
+                if instance.key_name is not None:
+                    instance_entity.add_attr(key='aws', subkey='ec2_key_name',
+                            value=instance.key_name)
+                if instance_entity not in ec2vmman.referencers():
+                    ec2vmman.allocate(instance_entity, 
+                            resource={
+                                'placement': instance.placement,
+                                'instance_id': instance.id })
         self.info('Finished, AWS objects should now be in the database')
 
 

d968b267dabb2510f51df099fa0814359b6759a2
Jorge A Gallegos <kad@blegh.net>
2012-10-23 14:31:55 -0700

    Bumping version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import sys
 
 setuptools.setup(
     name = "clusto-ec2",
-    version = "0.3",
+    version = "0.4",
     packages = setuptools.find_packages('src'),
     author = "Ron Gorodetzky",
     author_email = "ron@parktree.net",

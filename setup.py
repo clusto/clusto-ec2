d328ba7252917b32d006c08ce805ca4769004de2
Jorge Gallegos <kad@blegh.net>
2013-07-02 15:49:10 -0700

    Bumping version, this gets us halfway to VPC land

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import sys
 
 setuptools.setup(
     name = "clusto-ec2",
-    version = "0.4.1",
+    version = "0.4.5",
     packages = setuptools.find_packages('src'),
     author = "Ron Gorodetzky",
     author_email = "ron@parktree.net",
